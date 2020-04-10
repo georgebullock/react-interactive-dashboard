4fb00d1c5b08203c5ad2c72868a0f1655fa35510
George <george.bullock@gmx.de>
2020-04-10 14:40:26 +0200

    RID-001: Add typescript

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ const isTest = String(process.env.NODE_ENV) === 'test';
 module.exports = {
 	presets: [
 		['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
-		'@babel/preset-react'
+		'@babel/preset-react',
+		'@babel/preset-typescript'
 	],
 	plugins: ['@babel/plugin-proposal-class-properties']
 };
---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+	render() {
+		return <div className="message">Hello world</div>;
+	}
+}
+
+export default ProductList;

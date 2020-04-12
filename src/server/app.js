da6e83cdc3745fcbf23e16a9d9d9913facf3f6d6
George <george.bullock@gmx.de>
2020-04-12 17:39:05 +0200

    RID-002: add nodemon

--- src/server/app.js
+++ src/server/app.js
@@ -7,6 +7,8 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+const port = 9000
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 module.exports = app;

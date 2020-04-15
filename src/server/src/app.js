bc6495fc97b64ab273bba41f8e954bd7adc0b9be
George <george.bullock@gmx.de>
2020-04-15 14:33:13 +0200

    RID-003: Clean up app. Add mysql. Remove body parser

--- src/server/src/app.js
+++ src/server/src/app.js
@@ -1,24 +1,17 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const app = express();
-const port = 9000
+const port = 9000;
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", (req, res) => {});
 
-app.listen(port)
+app.listen(port);
 
 module.exports = app;

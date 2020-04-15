acd6e936d25c01783542a259697d1df99b5d6daa
George <george.bullock@gmx.de>
2020-04-15 15:03:40 +0200

    RID-003: Add test route

--- src/server/src/app.js
+++ src/server/src/app.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+	res.json({message: `Serving React Interactive Dashboard Server on port ${port}`});
+});
 
 app.listen(port);
 

10d4ba36f0ddb1b85cec8d5ea1e51a52b3913b2f
George <george.bullock@gmx.de>
2020-04-15 22:10:00 +0200

    RID-003: Add helmet security middleware

--- src/server/src/app.js
+++ src/server/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const helmet = require('helmet');
+const logger = require("morgan");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
 const app = express();
 const port = 9000;
 
 app.use(logger("dev"));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());

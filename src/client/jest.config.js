f2caeb0885efd763febbef6e8af09492c624b53c
George <george.bullock@gmx.de>
2020-04-18 21:40:46 +0200

    RID-004: Fix typo

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	roots: ['<rootDir>/src'],
-	moduleDirectories: ['node_module', 'src'],
+	moduleDirectories: ['node_modules', 'src'],
 	preset: 'ts-jest',
 	testEnvironment: 'node'
 };

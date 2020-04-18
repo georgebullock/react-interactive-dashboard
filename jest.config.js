8ae21632d3d723420fdf0cea4de629247f75bfde
George <george.bullock@gmx.de>
2020-04-19 01:25:46 +0200

    RID-004: Update ts configs. Add jest projects config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+{
+	projects: [
+		{
+			displayName: "CLIENT",
+			rootDir: '<rootDir>'/src/client/jest.config.js
+		}
+		{
+			displayName: "SERVER",
+			rootDir: '<rootDir>'/src/server/jest.config.js
+		}
+	]
+}

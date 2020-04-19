8017292f51579ae88f888e8266aac78e2df8a503
George <george.bullock@gmx.de>
2020-04-19 02:30:30 +0200

    RID-004: Minor config updates

--- src/server/jest.config.js
+++ src/server/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	roots: ['<rootDir>'],
 	moduleDirectories: ['node_modules', '<rootDir>/src'],
-	// preset: 'ts-jest',
-	testEnvironment: 'node'
+	preset: '',
+	testEnvironment: 'node',
+	testPathIgnorePatterns: ['/node_modules/', '/dist/']
 };

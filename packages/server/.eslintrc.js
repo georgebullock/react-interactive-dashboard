e07f0f4c56fff6ce0c36728b077058dac63eb9cf
George <george.bullock@gmx.de>
2020-05-14 12:21:38 +0200

    RID-005: Remove js parsing from eslint

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2019,
 		sourceType: 'module'
 	},
-	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'prettier'
+	],
 	rules: {
 		'prettier/prettier': 'error',
 		'no-console': 'off',
-		'no-unused-vars': 'off'
+		'no-unused-vars': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'error'
 	},
 	settings: {},
 	env: {
@@ -15,25 +24,5 @@ module.exports = {
 		node: true,
 		es6: true,
 		jest: true
-	},
-	overrides: [
-		{
-			files: '**/*.+(ts|tsx)',
-			parser: '@typescript-eslint/parser',
-			parserOptions: {
-				project: './tsconfig.json'
-			},
-			rules: {
-				'@typescript-eslint/explicit-function-return-type': 'error'
-			},
-			plugins: ['@typescript-eslint'],
-			extends: [
-				'eslint:recommended',
-				'plugin:@typescript-eslint/eslint-recommended',
-				'plugin:@typescript-eslint/recommended',
-				'plugin:@typescript-eslint/recommended-requiring-type-checking',
-				'plugin:prettier/recommended'
-			]
-		}
-	]
+	}
 };

217af92936c7b6b262a5b6b899ea98478d8e3f0c
George <george.bullock@gmx.de>
2020-07-03 19:13:01 +0200

    RID-007: Remove import alias

--- packages/server/src/models/dashboard.model.ts
+++ packages/server/src/models/dashboard.model.ts
@@ -1,9 +1,8 @@
 import { QueryResponse } from '../types/sqlQuery';
 import { sqlQuery } from '../utils/sqlQuery';
-import { Dashboard as IDashboard } from '../interfaces/Dashboard';
+import { Dashboard } from '../interfaces/Dashboard';
 
-// For now this module will only GET comment, user, and tag data for the dashboard page
-const DashboardModel: IDashboard = {
+const DashboardModel: Dashboard = {
 	getAllCommentsCount: (): Promise<QueryResponse> => {
 		const result = sqlQuery<QueryResponse>(
 			`select count(*) as count from comments`
